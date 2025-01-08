b8729f88f57fe6282e6b5e86e7b98f3639de62e2
MaybeCarina <ccnicole0405@gmail.com>
2025-01-08 18:32:43 -0500

    navbar fix again

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('body').insertAdjacentHTML('afterbegin', data);
+        });
+});
   
