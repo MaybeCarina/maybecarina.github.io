87b99c5c5e1e83d87cfda8a2872d3152be1210fa
MaybeCarina <ccnicole0405@gmail.com>
2025-01-08 19:16:02 -0500

    dont display page on in navbar 2

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
             navLinks.forEach(link => {
                 if (link.getAttribute('href') === currentPath) {
-                link.parentElement.classList.add('active'); 
+                    link.parentElement.classList.add('active'); 
                 }
             });
-            
+
             document.querySelector('.hamburger').addEventListener('click', () => {
                 document.querySelector('.nav-links').classList.toggle('active');
             });
